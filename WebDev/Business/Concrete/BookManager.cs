991e681e3987ee91e3e0b078341d39f93e563935
ertkzltnc <ertkiziltunc@gmail.com>
2020-04-18 23:34:33 +0300

    Bussiness

---
+++ WebDev/Business/Concrete/BookManager.cs
@@ -0,0 +1,51 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class BookManager : IBookService
+    {
+        private IBookDal _bookDal;
+
+        public BookManager(IBookDal bookDal)
+        {
+            _bookDal = bookDal;
+        }
+        public void Add(Book book)
+        {
+            _bookDal.Add(book);
+        }
+
+        public void Delete(Book book)
+        {
+            _bookDal.Delete(book);
+        }
+
+        public List<Book> GetAll()
+        {
+            return _bookDal.GetList().ToList();
+
+        }
+
+        public List<Book> GetByAuthorId(int id)
+        {
+            return _bookDal.GetList(x=>x.AuthorId==id).ToList();
+        }
+
+        public Book GetById(int id)
+        {
+            return _bookDal.Get(x => x.Id == id);
+        }
+
+        public void Update(Book book)
+        {
+            _bookDal.Update(book);
+        }
+    }
+}
